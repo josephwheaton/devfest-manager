29ac54d7c0942f05e294a1dc52e857b4c1592d30
Joseph Wheaton <jwhn91@gmail.com>
2025-12-10 11:32:00 -0600

    Further ssr, pre render individual event details

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,17 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
-  {
-    path: 'event/:id',
-    renderMode: RenderMode.Server,
-  },
   // {
   //   path: 'event/:id',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return [{ id: '1' }, { id: '2' }];
-  //   },
+  //   renderMode: RenderMode.Server,
   // },
+  // below: further optimizations
+  {
+    path: 'event/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [{ id: '1' }, { id: '2' }];
+    },
+  },
   {
     path: 'admin/create',
     renderMode: RenderMode.Client,
