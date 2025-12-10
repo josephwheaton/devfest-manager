b02d6b8fede34546502141e24d0b3310a18340fc
Joseph Wheaton <jwhn91@gmail.com>
2025-12-10 13:02:42 -0600

    midway d2m2

---
+++ src/app/shared/tabs/tab-state.ts
@@ -0,0 +1,12 @@
+// src/app/shared/tabs/tab-state.ts
+import { Injectable, signal } from '@angular/core';
+
+@Injectable() // No providedIn: 'root'!
+export class TabState {
+  // Tracks the label of the currently active tab
+  readonly activeTab = signal<string>('');
+
+  activate(label: string) {
+    this.activeTab.set(label);
+  }
+}
